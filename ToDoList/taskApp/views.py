5047227b8f9cdcf485ca33b4dc64334c39fcfee1
Zahid Hasan Milu <minions.milu889@gmail.com>
2023-04-05 02:21:12 +0600

    Create task, Delete Task

--- ToDoList/taskApp/views.py
+++ ToDoList/taskApp/views.py
@@ -1,26 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
+from django.contrib import messages
+
 # Create your views here.
 
 def home(request):
     tasks = Task.objects.all()
-
-
-
+    form = TaskForm()
+    if request.method=="POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
     context={
         'tasks': tasks,
-
+        'form': form,
     }
     return render(request, 'index.html', context)
 
 
+
+
 def update(request,id):
     task = Task.objects.get(id=id)
-    print(task)
-    return render(request, 'update.html')
+    if request.method=="POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            messages.error(request, "Please INPUT DATA")
+            return redirect('update')
+    else:
+        form = TaskForm(instance=task)
+    context={
+        'form': form,
+        'task': task,
+    }
+    return render(request, 'update.html', context)
 
 def delete(request,id):
     task = Task.objects.get(id=id)
-    print(task)
-    return render(request, 'update.html')
+    if request.method=='POST':
+        task.delete()
+        return redirect('home')
+    context={
+        'task': task,
+    }
+    return render(request, 'delete.html', context)
