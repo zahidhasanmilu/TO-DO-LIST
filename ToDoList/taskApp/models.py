2c6255de29b8a410f3359bfc63854c91f83191a3
Zahid Hasan Milu <minions.milu889@gmail.com>
2023-04-06 22:17:45 +0600

    task update show

--- ToDoList/taskApp/models.py
+++ ToDoList/taskApp/models.py
@@ -7,6 +7,7 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
         return self.title + " , " + "COMPLETE : " + str(self.complete)
