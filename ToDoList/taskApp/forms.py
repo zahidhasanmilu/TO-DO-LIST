707dce042a0f1a56f7ad7a3971e039744d2355ce
Zahid Hasan Milu <minions.milu889@gmail.com>
2023-11-05 03:54:43 +0600

    initial commit

--- ToDoList/taskApp/forms.py
+++ ToDoList/taskApp/forms.py
@@ -11,4 +11,9 @@ class TaskForm(forms.ModelForm):
 class CreateTaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title',]
+        fields = ['title','description']
+
+        widgets = {
+            'description': forms.Textarea(attrs={'placeholder': 'You can discribe your task here'}),
+            'title': forms.TextInput(attrs={'placeholder': 'Enter task title here'}),
+        }
--- ToDoList/taskApp/models.py
+++ ToDoList/taskApp/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True, blank=True, null=True)
